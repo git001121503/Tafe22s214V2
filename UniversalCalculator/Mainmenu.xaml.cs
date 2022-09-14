2df19dc6aad4ee425ea7135ca6d5f6f5d12f8baa
Minh Ong <minh.ong@student.tafesa.edu.au>
2022-09-15 00:56:59 +0930

    Commit added Main menu .xaml and cs files, modified .csproj file

---
+++ UniversalCalculator/Mainmenu.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Enumeration;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Popups;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Calculator
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class Mainmenu : Page
+	{
+		public Mainmenu()
+		{
+			this.InitializeComponent();
+		}
+
+		private void mathButton_Click(object sender, RoutedEventArgs e)
+		{
+			this.Frame.Navigate(typeof(MainPage));
+		}
+
+
+		private void mortgageButton_Click(object sender, RoutedEventArgs e)
+		{
+			this.Frame.Navigate(typeof(MortgageCalc));
+		}
+
+		private void exitButton_Click(object sender, RoutedEventArgs e)
+		{
+			System.Environment.Exit(0);
+		}
+
+		private void currencyButton_Click(object sender, RoutedEventArgs e)
+		{
+			this.Frame.Navigate(typeof(CurrencyConversionCalculator));
+		}
+	}
+}
